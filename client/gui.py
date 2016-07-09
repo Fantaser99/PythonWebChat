56de93debaa7df4f41278e53e9905ac3a2f5b01a
AlexFSmirnov <alex.f.smirnov@yandex.ru>
2016-07-09 22:06:35 +0300

    Changed log type from Text to CustomText in order to highlight nicknames in the future

--- client/gui.py
+++ client/gui.py
@@ -1,6 +1,43 @@
 from tkinter import *
 import time
 
+class CustomText(Text):
+    '''A text widget with a new method, highlight_pattern()
+    
+    example:
+    
+    text = CustomText()
+    text.tag_configure("red", foreground="#ff0000")
+    text.highlight_pattern("this should be red", "red")
+    
+    The highlight_pattern method is a simplified python
+    version of the tcl code at http://wiki.tcl.tk/3246
+    '''
+    def __init__(self, *args, **kwargs):
+        Text.__init__(self, *args, **kwargs)
+    
+    def highlight_pattern(self, pattern, tag, start="1.0", end="end",
+                          regexp=False):
+        '''Apply the given tag to all text that matches the given pattern
+    
+        If 'regexp' is set to True, pattern will be treated as a regular
+        expression.
+        '''
+    
+        start = self.index(start)
+        end = self.index(end)
+        self.mark_set("matchStart", start)
+        self.mark_set("matchEnd", start)
+        self.mark_set("searchLimit", end)
+    
+        count = IntVar()
+        while True:
+            index = self.search(pattern, "matchEnd","searchLimit",
+                                count=count, regexp=regexp)
+            if index == "": break
+            self.mark_set("matchStart", index)
+            self.mark_set("matchEnd", "%s+%sc" % (index, count.get()))
+            self.tag_add(tag, "matchStart", "matchEnd")
 
 def setStatus(text):
     statusbar.config(text=str(text))
@@ -21,7 +58,7 @@ root = Tk()
 
 WIDTH = 15  # Divisible by 15.
 
-log = Text(root)
+log = CustomText(root)
 log.grid(row=0, column=0, columnspan=WIDTH // 15 * 14, padx=1, pady=1)
 
 users = Listbox(root)
